a8a950c16596cbc54f32f8a2f5fcb09e3afcf57e
Rafi Priatna K <rafipriatnak@gmail.com>
2021-07-04 11:18:53 +0700

    Edit produk gak sengaja kehapus bjir

---
+++ resources/views/pages/admin/product/edit.blade.php
@@ -0,0 +1,165 @@
+@extends('layouts.admin')
+
+@section('title')
+Dashboard Product
+@endsection
+
+@section('content')
+<div class="section-content section-dashboard-home" data-aos="fade-up">
+    <div class="container-fluid">
+        <div class="dashboard-heading">
+            <h2 class="dashboard-title">Product</h2>
+            <p class="dashboard-subtitle">
+                Edit Product
+            </p>
+        </div>
+        <div class="dashboard-content">
+            <div class="row">
+                <div class="col-md-12">
+                    @if($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="{{ route('product.update', $item->id) }}" method="POST" enctype="multipart/form-data">
+                                @method('PUT')
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Nama Produk</label>
+                                            <input type="text" name="name" class="form-control" value="{{ $item->name }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Kategori Produk</label>
+                                            <option value="{{ $item->categories }}" selected>{{ $item->category->name }}</option>
+                                            <select name="categories_id" class="form-control">
+                                                @foreach ($categories as $category)
+                                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Stok Produk</label>
+                                            <input type="number" name="stock" class="form-control" value="{{ $item->stock }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Berat Produk</label>
+                                            <input type="number" name="weight" class="form-control" value="{{ $item->weight }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Harga Produk</label>
+                                            <input type="number" name="price" class="form-control" value="{{ $item->price }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Deskripsi Produk</label>
+                                            <textarea name="description" id="editor">{!! $item->description !!}</textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col text-right">
+                                        <button type="submit" class="btn btn-success px-5">
+                                            Save Now
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="card my-4">
+                        <div class="card-body">
+
+                            <div class="row mb-4">
+                                <div class="col">
+                                    <h5 class="card-title">Variant</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">Tambahkan variasi warna produk di sini.</h6>
+                                </div>
+                                <div class="col text-right my-auto">
+                                    <a href="{{ route('createVariant', $item->id) }}" class="btn btn-primary py-2 px-4">
+                                        Add Variant
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="table-responsive">
+                                <table class="table table-hover scroll-horizontal-vertical w-100" id="crudTable">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Foto</th>
+                                            <th>Warna</th>
+                                            <th>Keterangan</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('addon-script')
+<script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace('editor');
+    var datatable = $('#crudTable').DataTable({
+        processing: true,
+        serverSide: true,
+        ordering: true,
+        ajax: {
+            url: '{!! url()->current() !!}',
+        },
+
+
+        columns: [{
+                data: 'id',
+                name: 'id'
+            },
+            {
+                data: 'photos',
+                name: 'photos'
+            },
+            {
+                data: 'color',
+                name: 'color'
+            },
+            {
+                data: 'description',
+                name: 'description'
+            },
+            {
+                data: 'action',
+                name: 'action',
+                orderable: false,
+                searcable: false,
+                width: '15%'
+            },
+        ]
+    });
+</script>
+@endpush
