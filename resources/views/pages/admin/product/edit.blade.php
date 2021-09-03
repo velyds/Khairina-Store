26234338bb75438680379c623e08d4f1a04f6ee7
Vely Dwiana Sari <velydwianasari18@gmail.com>
2021-09-03 23:50:55 +0700

    variant

--- resources/views/pages/admin/product/edit.blade.php
+++ resources/views/pages/admin/product/edit.blade.php
@@ -94,7 +94,7 @@ Dashboard Product
                                 </div>
                                 <div class="col text-right my-auto">
                                     <a href="{{ route('createVariant', $item->id) }}" class="btn btn-primary py-2 px-4">
-                                        Add Variant
+                                        Tambah Varian
                                     </a>
                                 </div>
                             </div>
